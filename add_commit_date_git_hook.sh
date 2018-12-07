3803f3447a26f2cd0bdb7693ef2e482fcfb80539
masterAndrey <filbert@yandex.ru>
2018-12-07 06:36:55 +0300

    version

--- add_commit_date_git_hook.sh
+++ add_commit_date_git_hook.sh
@@ -3,5 +3,5 @@
 # Creates file with git commit datetime so application can show current version 'build' time
 # Use hook_install.sh to install
 #
-date "+%F %T" > src/biuld_timestamp
-git add src/biuld_timestamp
+date "+%F %T" > src/build_timestamp
+git add src/build_timestamp
---
+++ src/version.py
@@ -0,0 +1,15 @@
+"""
+Returns version as 'build' time from file build_timestamp
+See add_commit_date_git_hook.sh and hook_install.sh
+"""
+
+
+def version():
+    try:
+        return open('build_timestamp', 'r').read().strip()
+    except:
+        return None
+
+
+if __name__ == '__main__':
+    print(version())
