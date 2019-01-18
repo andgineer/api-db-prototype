a9b9fa9a20fa416c11d5bff73fdea2b9e8e36b65
masterAndrey <filbert@yandex.ru>
2019-01-18 16:41:28 +0300

    auto inc build num

--- git_hook_add_commit_date.sh
+++ git_hook_add_commit_date.sh
@@ -3,7 +3,14 @@
 # Creates file with git commit datetime so application can show current version 'build' time
 # Use git_hook_install.sh to install
 #
-FILE=src/build_timestamp
-date "+%F %T" > "$FILE"
+DATE_FILE=src/build_timestamp
+VER_FILE=src/build_ver
+date "+%F %T" > "$DATE_FILE"
+v_h=$(cat $VER_FILE | awk -F "." '{print $1 "." $2}')
+v_m=$(cat $VER_FILE | awk -F "." '{print $3}')
+v_m=$((v_m+1))
+echo $v_h
+echo $v_m
+echo "$v_h.$v_m" > "$VER_FILE"
 #cat "$1" >> "$FILE" this is for commit-msg but I do not see how to add file to commit at this stage
-git add "$FILE"
+git add "$DATE_FILE" "$VER_FILE"
