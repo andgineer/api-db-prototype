588a69098f8bedd88621e5c3ee9f8012020f7d71
masterAndrey <filbert@yandex.ru>
2019-03-19 05:57:46 +0300

    remove e2e tests to separate repository

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@
 # To filter by test name use test.sh -k <test name>
 # To filter by test marks use test.sh -m "mark1 and mark2"
 #
-python3.7 -m pytest -s -v -W ignore::DeprecationWarning tests $@
+python3.7 -m pytest -s -v -W ignore::DeprecationWarning $@
 
