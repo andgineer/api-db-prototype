2915948e271d604870ca2524c79aa160035ee704
Andrei Sorokin2 <Andrei_Sorokin2@epam.com>
2019-04-08 13:18:32 +0300

    environ_base

--- tests/api.py
+++ tests/api.py
@@ -12,8 +12,53 @@ import contextlib
 
 client = None  # Injection for client to send HTTP requests. It can be flask test client or requests
 api_url = None  # Injection for api url. It's relational for flask test client, and this is full path for requests.
+verify_ssl = None  # Set it only for requests
 
 last_response = None  # Last response to api call
+
+
+def add_verify(kwargs):
+    if verify_ssl is not None:
+        kwargs['verify'] = verify_ssl
+    return kwargs
+
+
+def post(*args, env=None, **kwargs):
+    # if 'verify' in kwargs:
+    #     del kwargs['verify']
+    if env is None:
+        env = {}
+    else:
+        env = {'environ_base': env}
+    return client.post(*args, **env, **add_verify(kwargs))
+
+
+def get(*args, env=None, **kwargs):
+    # if 'verify' in kwargs:
+    #     del kwargs['verify']
+    if env is None:
+        env = {}
+    else:
+        env = {'environ_base': env}
+    return client.get(*args, **env, **add_verify(kwargs))
+
+
+def put(*args, **kwargs):
+    # if 'verify' in kwargs:
+    #     del kwargs['verify']
+    return client.put(*args, **add_verify(kwargs))
+
+
+def delete(*args, **kwargs):
+    # if 'verify' in kwargs:
+    #     del kwargs['verify']
+    return client.delete(*args, **add_verify(kwargs))
+
+
+def options(*args, **kwargs):
+    # if 'verify' in kwargs:
+    #     del kwargs['verify']
+    return client.options(*args, **add_verify(kwargs))
 
 
 def headers(token=None):
@@ -52,7 +97,7 @@ def parse_api_reply(resp, expected_statuses=HttpCode.successes) -> dict:
 
 
 def get_token(email, password, expected_statuses=HttpCode.successes):
-    resp = client.post(
+    resp = post(
         f'{api_url}/auth',
         data=json.dumps({'email': email, 'password': password}),
         headers=headers()
@@ -66,18 +111,18 @@ def get_token(email, password, expected_statuses=HttpCode.successes):
 
 def create_user(token, user, expected_statuses=HttpCode.successes, patch_email=True):
     with patch('cloud_services.send_email', return_value=None) if patch_email else contextlib.nullcontext():
-        resp = client.post(f'{api_url}/users', data=json.dumps({'new_user': user}), headers=headers(token))
+        resp = post(f'{api_url}/users', data=json.dumps({'new_user': user}), headers=headers(token))
     return parse_api_reply(resp, expected_statuses=expected_statuses)
 
 
 def get_user(token, id, expected_statuses=HttpCode.successes):
-    resp = client.get(f'{api_url}/users/{id}', headers=headers(token))
+    resp = get(f'{api_url}/users/{id}', headers=headers(token))
     return parse_api_reply(resp, expected_statuses=expected_statuses)
 
 
 def update_user(token, user, id, expected_statuses=HttpCode.successes, patch_email=True):
     with patch('cloud_services.send_email', return_value=None) if patch_email else contextlib.nullcontext():
-        resp = client.put(f'{api_url}/users/{id}', data=json.dumps(user), headers=headers(token))
+        resp = put(f'{api_url}/users/{id}', data=json.dumps(user), headers=headers(token))
     return parse_api_reply(resp, expected_statuses=expected_statuses)
 
 
@@ -85,7 +130,7 @@ def users_list(token, expected_statuses=HttpCode.successes, **kwargs):
     url = f'{api_url}/users'
     if kwargs:
         url += f'?{urllib.parse.urlencode(kwargs)}'
-    resp = client.get(url, headers=headers(token))
+    resp = get(url, headers=headers(token))
     data = parse_api_reply(resp, expected_statuses=expected_statuses)
     if expected_statuses == HttpCode.successes:
         return data['data']
@@ -94,11 +139,11 @@ def users_list(token, expected_statuses=HttpCode.successes, **kwargs):
 
 
 def delete_user(token, id, expected_statuses=HttpCode.successes):
-    resp = client.delete(f'{api_url}/users/{urllib.parse.quote(str(id))}', headers=headers(token))
+    resp = delete(f'{api_url}/users/{urllib.parse.quote(str(id))}', headers=headers(token))
     parse_api_reply(resp, expected_statuses=expected_statuses)
 
 
 def get_options(path):
-    resp = client.options(f'{api_url}/{path}')
+    resp = options(f'{api_url}/{path}')
     assert resp.status_code in [200, 204]
     return resp
