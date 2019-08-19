892ab17abd9a591fdb15cea78079f2dc46b4ad21
andgineer <filbert@yandex.ru>
2019-08-19 21:07:18 +0300

    coverall andgineer

--- coverage.sh
+++ coverage.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3.7 -m pytest -s -v --ignore src/alembic --ignore src/app.py --ignore swagger-codegen --doctest-modules src -v --cov src --cov-report term-missing -W ignore::DeprecationWarning
+python3.7 -m pytest -s -v --doctest-modules --cov src --cov-report term-missing --ignore src/alembic --ignore swagger-codegen --ignore src/flask_server/api_app.py -W ignore::DeprecationWarning
