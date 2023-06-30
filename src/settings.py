e5416804b7782d6bd5d274a60bd8a2ebc3cf6fce
andgineer <Andrei_Sorokin2@epam.com>
2023-06-30 10:43:58 +0200

    https://github.com/andgineer/api-db-prototype/issues/15 mypy 2023-06-30

--- src/settings.py
+++ src/settings.py
@@ -4,7 +4,8 @@ import tempfile
 import urllib.parse
 from abc import abstractmethod
 from datetime import timezone
-from typing import Optional
+from typing import Any, Optional
+from unittest.mock import MagicMock
 
 import journaling
 from flask_server.api_app import app as flask_app
@@ -30,8 +31,10 @@ TEST_LOG_CONFIG_FILE_PATH = "tests/resources/logging.yaml"
 
 
 class ConfigBase:
-    jwt_secret_key_file = None
-    jwt_public_key_file = None
+    """Base class for config."""
+
+    jwt_secret_key_file: Optional[str] = None
+    jwt_public_key_file: Optional[str] = None
 
     token_expiration_delta = datetime.timedelta(hours=TOKEN_EXPIRATION_HOURS)
 
@@ -40,158 +43,157 @@ class ConfigBase:
 
     web_enableCrossOriginRequests = False
 
-    _port = DEFAULT_PORT
-    _db_uri = None
+    _port: str = str(DEFAULT_PORT)
+    _db_uri: Optional[str] = None
 
     db_autometa = True  # refresh DB metadata at start
     db_sqltrace = False
     profiler_cprofile = True  # activates profiling.py#analyze() context manager
 
-    api_host = None
+    api_host: Optional[str] = None
     api_root = ""
 
     aws_region = "us-east-1"
 
     def __init__(self, log_config: Optional[str] = None):
+        """Init config."""
         journaling.setup(log_config)
 
     @property
-    def api_url(self):
-        return urllib.parse.urljoin(self.api_host, self.api_root)
+    def api_url(self) -> str:
+        """API url."""
+        return urllib.parse.urljoin(self.api_host, self.api_root)  # type: ignore
 
     @property
     @abstractmethod
-    def app(self):
-        """
-        Flask-compatible App server to run.
-        """
+    def app(self) -> Any:
+        """Flask-compatible App server."""
 
     @property
-    def db_uri(self):
-        """
-        DB URI: engine://user:password@path.
+    def db_uri(self) -> str:
+        """DB URI: engine://user:password@path.
+
         For example:
             postgres://user@example.com/template1
             sqlite:////data/test.sqlite
         """
         if self._db_uri:
             return self._db_uri
-        else:
-            raise ValueError("No DB URI specified")
+        raise ValueError("No DB URI specified")
 
     @db_uri.setter
-    def db_uri(self, value):
+    def db_uri(self, value: str) -> None:
+        """DB URI."""
         self._db_uri = value
 
     @property
-    def port(self):
+    def port(self) -> str:
+        """App port."""
         return self._port
 
     @port.setter
-    def port(self, value):
+    def port(self, value: str) -> None:
+        """App port."""
         self._port = value
 
     @staticmethod
-    def now():
-        """
-        To have one place to decide whether we use utc or server timezone.
+    def now() -> datetime.datetime:
+        """To have one place to decide whether we use utc or server timezone.
+
         And for ease of mocking in expiration tests.
         """
         return datetime.datetime.now(timezone.utc)
 
-    def from_date_str(self, date_str: str) -> datetime:
-        """
-        Converts date string into datetime
-        """
+    def from_date_str(self, date_str: str) -> datetime.datetime:
+        """Convert date string into datetime."""
         return self.rfc3339_to_date(date_str)
 
     @staticmethod
-    def date_to_rfc3339(date: datetime) -> str:
-        """
-        Date as 'yyyy-mm-dd'
-        """
+    def date_to_rfc3339(date: datetime.datetime) -> str:
+        """Date as 'yyyy-mm-dd'."""
         return date.isoformat(sep="T").split("T")[0]
 
     @staticmethod
-    def rfc3339_to_date(date_str: str) -> datetime:
-        """
-        'yyyy-mm-dd' (UTC) to datetime
-        """
+    def rfc3339_to_date(date_str: str) -> datetime.datetime:
+        """'yyyy-mm-dd' (UTC) to datetime."""
         return datetime.datetime.strptime(date_str, "%Y-%m-%d").replace(tzinfo=timezone.utc)
 
 
 class ConfigTest(ConfigBase):
-    """
-    Creates temp sqlite db.
-    """
+    """Creates temp sqlite db."""
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """Init."""
         super().__init__(TEST_LOG_CONFIG_FILE_PATH)
         self.db_file_object, self.db_file_name = tempfile.mkstemp()
-        self.db_uri = "sqlite:///" + self.db_file_name
+        self.db_uri = f"sqlite:///{self.db_file_name}"
         self.jwt_public_key_file = "src/secret/jwt_certificate.pem"
         self.jwt_secret_key_file = "src/secret/jwt_private.key"
         self.api_host = "http://localhost:5000"
 
-    def __del__(self):
+    def __del__(self) -> None:
+        """Remove temp db file."""
         if os is not None:
             os.close(self.db_file_object)
             os.remove(self.db_file_name)
 
+    @property
+    def app(self) -> Any:
+        """Flask-compatible App server."""
+        return MagicMock()
+
 
 class ConfigTestPureFlask(ConfigTest):
-    """
-    Creates temp sqlite db.
-    """
+    """Creates temp sqlite db."""
 
     @property
-    def app(self):
+    def app(self) -> Any:
+        """Flask-compatible App server."""
         return flask_app
 
 
 class ConfigTestTransmute(ConfigTest):
-    """
-    Creates temp sqlite db.
-    """
+    """Creates temp sqlite db."""
 
     @property
-    def app(self):
+    def app(self) -> Any:
+        """Flask-compatible App server."""
         return transmute_app
 
 
 class ConfigTestConnexion(ConfigTest):
-    """
-    Creates temp sqlite db.
-    """
+    """Creates temp sqlite db."""
 
     @property
-    def app(self):
+    def app(self) -> Any:
+        """Flask-compatible App server."""
         return connexion_app.app
 
 
 class ConfigDev(ConfigBase):
-    """
-    Development environment.
+    """Development environment.
+
     Local sqlite DB.
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """Init."""
         super().__init__()
         self.db_uri = "sqlite:///../debug_db.sqlite"
         self.jwt_public_key_file = PUBLIC_JWT_KEY_FILE
         self.jwt_secret_key_file = PRIVATE_JWT_KEY_FILE
 
     @property
-    def app(self):
+    def app(self) -> Any:
+        """Flask-compatible App server."""
         return transmute_app
 
 
 class ConfigProd(ConfigBase):
-    """
-    Production.
-    """
+    """Production."""
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """Init."""
         super().__init__()
         self.db_uri = os.environ.get(DB_URI_ENV, "sqlite:///../debug_db.sqlite")
         self.port = os.environ.get(PORT_ENV, self.port)
@@ -201,19 +203,19 @@ class ConfigProd(ConfigBase):
         self.api_host = "https://example.com"
 
     @property
-    def app(self):
+    def app(self) -> Any:
+        """Flask-compatible App server."""
         return flask_app
 
 
 class ConfigTestWrong(ConfigBase):
-    """
-    To test wrong config
-    """
+    """To test wrong config."""
 
     @property
-    def app(self):
+    def app(self) -> Any:
+        """Flask-compatible App server."""
         return flask_app
 
 
-config: ConfigBase = None  # Convenient config injection
+config: Optional[ConfigBase] = None  # Convenient config injection
 # app is responsible to populate this var with config object
