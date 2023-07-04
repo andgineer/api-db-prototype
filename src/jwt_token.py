9a5046b85a37d6ad2e51cbe873b85ec5da73db00
andgineer <Andrei_Sorokin2@epam.com>
2023-07-04 06:54:15 +0200

    https://github.com/andgineer/api-db-prototype/issues/15 mypy 2023-07-04 #2

--- src/jwt_token.py
+++ src/jwt_token.py
@@ -1,10 +1,10 @@
-"""
-JWT cryptography.
+"""JWT cryptography.
 
 Checked with https://jwt.io/#debugger-io
 """
 import datetime
 from datetime import timezone
+from typing import Any, Dict, Optional
 
 import jwt
 from cryptography.hazmat.backends import default_backend
@@ -20,62 +20,54 @@ JWT_MIN_LENGTH = 50
 
 
 class JwtCrypto:
-    """
-    Jwt encoder/decoder
-    """
+    """JWT encoder/decoder."""
 
-    _public_key: bytes = None
-    _private_key: bytes = None
+    _public_key: Optional[bytes] = None
+    _private_key: Optional[bytes] = None
 
     @property
-    def public_key(self):
-        """
-        For lazy loading so we can set key's files beforehand
-        """
+    def public_key(self) -> bytes:
+        """Facilitate lazy loading so we can set key's files beforehand."""
         if not self._public_key:
             log.debug(f"Loading public key from {settings.config.jwt_public_key_file}")
             cert_str = open(settings.config.jwt_public_key_file, "rb").read()
             cert_obj = load_pem_x509_certificate(cert_str, default_backend())
-            self._public_key = cert_obj.public_key()
-        return self._public_key
+            self._public_key = cert_obj.public_key()  # type: ignore
+        return self._public_key  # type: ignore
 
     @property
-    def private_key(self):
-        """
-        For lazy loading so we can set key's files beforehand
-        """
+    def private_key(self) -> bytes:
+        """Facilitate lazy loading so we can set key's files beforehand."""
         if not self._private_key:
             log.debug(f"Loading private key from {settings.config.jwt_secret_key_file}")
             self._private_key = open(settings.config.jwt_secret_key_file, "rb").read()
         return self._private_key
 
-    def encode(self, payload: dict) -> str:
+    def encode(self, payload: Dict[str, Any]) -> str:
+        """Encode payload into jwt string."""
         return jwt.encode(payload, self.private_key, algorithm="RS256")
 
-    def decode(self, encoded: str) -> dict:
+    def decode(self, encoded: str) -> Optional[Dict[str, Any]]:
+        """Decode jwt string into payload."""
         try:
             decoded = jwt.decode(
                 encoded,
                 self.public_key,
-                algorithms="RS256",
+                algorithms=["RS256"],
             )
         except PyJWTError as e:
             decoded = None
             log.debug(f'Decode error for jwt "{encoded}": {e}')
-        return decoded
+        return decoded  # type: ignore
 
     @staticmethod
-    def datetime2jwt(utc_time):
-        """
-        Converts utc_time into POSIX (assuming that utc_time is in utc timezone)
-        """
-        return str(utc_time.timestamp()).split(".")[0]
+    def datetime2jwt(utc_time: datetime.datetime) -> str:
+        """Convert utc_time into POSIX (assuming that utc_time is in utc timezone)."""
+        return str(utc_time.timestamp()).split(".", maxsplit=1)[0]
 
     @staticmethod
-    def jwt2datetime(jwt_time):
-        """
-        Converts POSIX time into utc datetime
-        """
+    def jwt2datetime(jwt_time: str) -> datetime.datetime:
+        """Convert POSIX time into utc datetime."""
         return datetime.datetime.fromtimestamp(int(jwt_time), tz=timezone.utc)
 
 
