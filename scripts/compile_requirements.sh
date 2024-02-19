47c784e513e2e06eb1ad095727be0addf3afa49b
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-02-19 08:09:53 +0100

    timing pip compile

--- scripts/compile_requirements.sh
+++ scripts/compile_requirements.sh
@@ -3,6 +3,18 @@
 # Pin current dependencies versions.
 #
 
+START_TIME=$(date +%s)
+
 uv pip compile requirements.dev.in
+REQS_DEV_TIME=$(date +%s)
+
 uv pip compile requirements.in
+REQS_TIME=$(date +%s)
+
 uv pip compile requirements.prod.in
+END_TIME=$(date +%s)
+
+echo "Req‘s dev compilation time: $((REQS_DEV_TIME - $START_TIME)) seconds"
+echo "Req‘s compilation time: $((REQS_TIME - $REQS_DEV_TIME)) seconds"
+echo "Req‘s prod compilation time: $((END_TIME - $REQS_TIME)) seconds"
+echo "Total execution time: $((END_TIME - $START_TIME)) seconds"
